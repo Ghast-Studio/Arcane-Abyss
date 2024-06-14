5c238ce8dcb0fad4fc561d9c22d3e383756e025c
Giingerbread <108549003+Giingerbread@users.noreply.github.com>
2024-06-14 19:25:47 +0200

    first commit

---
+++ src/main/java/net/headnutandpasci/arcaneabyss/ArcaneAbyss.java
@@ -0,0 +1,17 @@
+package net.headnutandpasci.arcaneabyss;
+
+import net.fabricmc.api.ModInitializer;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+public class ArcaneAbyss implements ModInitializer {
+	public static final String MOD_ID = "arcaneabyss";
+    public static final Logger LOGGER = LoggerFactory.getLogger(MOD_ID);
+
+	@Override
+	public void onInitialize() {
+
+		LOGGER.info("Hello Fabric world!");
+	}
+}
---
+++ src/main/java/net/headnutandpasci/arcaneabyss/ArcaneAbyssModClient.java
@@ -0,0 +1,10 @@
+package net.headnutandpasci.arcaneabyss;
+import net.fabricmc.api.ClientModInitializer;
+
+public class ArcaneAbyssModClient implements ClientModInitializer {
+    @Override
+    public void onInitializeClient(){
+        
+    }
+
+}

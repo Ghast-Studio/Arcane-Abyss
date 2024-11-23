02677e25005ea814a8f6396470e22568255a53cf
Luca Leukert <derluleu@gmail.com>
2024-11-23 19:36:31 +0100

    remove testing mixin and add GENERIC_KNOCKBACK_RESISTANCE to Slimeviathan

--- src/main/java/net/headnutandpasci/arcaneabyss/entity/slime/boss/slimeviathan/SlimeviathanEntity.java
+++ src/main/java/net/headnutandpasci/arcaneabyss/entity/slime/boss/slimeviathan/SlimeviathanEntity.java
@@ -75,6 +75,7 @@ public class SlimeviathanEntity extends ArcaneSlimeEntity implements SkinOverlay
     public static DefaultAttributeContainer.Builder setAttributesGreenSlime() {
         return AnimalEntity.createMobAttributes()
                 .add(EntityAttributes.GENERIC_MAX_HEALTH, 800.0f)
+                .add(EntityAttributes.GENERIC_KNOCKBACK_RESISTANCE, 1.0f)
                 .add(EntityAttributes.GENERIC_ATTACK_DAMAGE, 20.0f)
                 .add(EntityAttributes.GENERIC_ATTACK_SPEED, 2.0f)
                 .add(EntityAttributes.GENERIC_MOVEMENT_SPEED, 0.4f)
